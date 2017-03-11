7752cdbffbb12e79c067c2681e1786f1139cf81c
censam <wgsampath@gmail.com>
2017-03-11 22:27:53 +0530

    react props and parent child relationships

--- imports/client/App.js
+++ imports/client/App.js
@@ -2,15 +2,31 @@ import React, {Component} from 'react';
 
 let hello="Meteor";
 
-
 export default class App extends Component{
+	constructor(){
+		super();
+		this.state = {
+			count:0
+		}
+	}
 
 	headingClick(){
-		console.log('asdf');
+		this.setState({count:this.state.count+1});
 	}
 	render(){
 		return (
-			<h1 onClick={this.headingClick}>Hello {hello}</h1>
+			<header onClick={this.headingClick.bind(this)}>
+			<Heading count={this.state.count+1} />
+			<Heading count={this.state.count} />
+			<Heading count={this.state.count+3} />
+			</header>
 			)
 	};
 } 
+
+
+class Heading extends Component{
+	render(){
+		return <h1>{this.props.count}</h1>
+	}
+}
