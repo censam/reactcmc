fe0be2cfc61003d8466e9bcc0adef4afd2a02b57
censam <wgsampath@gmail.com>
2017-03-21 16:43:27 +0530

    Updating Collection and records

--- imports/client/App.js
+++ imports/client/App.js
@@ -7,6 +7,26 @@ let hello="Meteor";
 
 class App extends Component{
 	
+	addItems(event){
+		event.preventDefault();
+		const itemOne = this.refs.itemOne.value.trim();
+		const itemTwo = this.refs.itemTwo.value.trim();
+		if((itemOne != '') && (itemTwo != '')){
+			Items.insert({
+				itemOne:{
+					text:itemOne,
+					value:0
+				},
+				itemTwo:{
+					text:itemTwo,
+					value:0
+				}, 
+			});
+			
+		}
+		this.refs.itemOne.value ='';
+		this.refs.itemTwo.value ='';
+	}
 	render(){
 		return (
 			<div>
@@ -14,8 +34,13 @@ class App extends Component{
 				<h1>Voting App</h1>
 			</header>
 			<main>
+				<form className="new-items" onSubmit={this.addItems.bind(this)}>
+				<input type ="text" ref='itemOne'/>
+				<input type ="text" ref='itemTwo'/>
+				<button type ="submit" >Submit</button>
+				</form>
 			{this.props.items.map((item)=>{
-				return <Item item={item} />
+				return <Item item={item} key={item._id} />
 
 			})}
 			</main>
--- server/main.js
+++ server/main.js
@@ -1,13 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import Items from '../imports/api/Items';
-
-
 Meteor.startup(() => {
-
-Items.insert({
-	name:'NameKey',
-	value:'NameValue'
-});
 	
-
 });
